e3297c25683d4e8490288d1588986abd0b994b3f
dingxin <wdx7266@vip.qq.com>
2019-04-16 15:28:56 +0800

    initialize project

---
+++ src/main/java/com/github/ontio/GovernanceSync/GovernanceSyncApplication.java
@@ -0,0 +1,13 @@
+package com.github.ontio.GovernanceSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernanceSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernanceSyncApplication.class, args);
+	}
+
+}
