725a43e51adc3d580b0e1abe33fb7ada2dcbfd4a
dingxin <wdx7266@vip.qq.com>
2019-04-18 19:00:26 +0800

    add controller

---
+++ src/main/java/com/github/ontio/GovernanceSync/controller/NodeController.java
@@ -0,0 +1,10 @@
+package com.github.ontio.governancesync.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/node")
+public class NodeController {
+
+}
