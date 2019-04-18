609d90c5694e273de8472bd5655878f2cd14a728
dingxin <wdx7266@vip.qq.com>
2019-04-18 18:57:06 +0800

    rename package

--- src/main/java/com/github/ontio/GovernanceSync/GovernanceSyncApplication.java
+++ src/main/java/com/github/ontio/GovernanceSync/GovernanceSyncApplication.java
@@ -1,13 +1,15 @@
-package com.github.ontio.GovernanceSync;
+package com.github.ontio.governancesync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class GovernanceSyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GovernanceSyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GovernanceSyncApplication.class, args);
+    }
 
 }
