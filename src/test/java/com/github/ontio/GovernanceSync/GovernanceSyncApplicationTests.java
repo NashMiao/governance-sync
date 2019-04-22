e1e1b5c57d7d97fd6efa705d4e8ffcee0cab9871
dingxin <wdx7266@vip.qq.com>
2019-04-22 13:58:15 +0800

    add unit test

--- src/test/java/com/github/ontio/GovernanceSync/GovernanceSyncApplicationTests.java
+++ src/test/java/com/github/ontio/GovernanceSync/GovernanceSyncApplicationTests.java
@@ -2,15 +2,28 @@ package com.github.ontio.governancesync;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class GovernanceSyncApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getNodeInfoLists() throws Exception {
+        this.mockMvc.perform(get("/v1/node/infoLists")).andDo(print()).andExpect(status().isOk());
+    }
 
 }
